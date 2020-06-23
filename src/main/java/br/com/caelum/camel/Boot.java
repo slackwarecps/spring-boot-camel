1df2fe4036b35e31739cfa3fd9bf4edbdfa7e858
faapereira <faapereira@faapereira10.indra.es>
2020-06-23 16:15:54 -0300

    opção em caso de herança da classe produto

--- src/main/java/br/com/caelum/camel/Boot.java
+++ src/main/java/br/com/caelum/camel/Boot.java
@@ -5,6 +5,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Boot {
+	
+	
+	
+//	@Bean
+//	public RoutesBuilder rota() { 
+//	    return new RouteBuilder() {
+//	        @Override
+//	        public void configure() throws Exception {
+//	            from("file:pedidos").
+//	            to("activemq:queue:pedidos");
+//	        } 
+//	    };
+//	}
+
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Boot.class, args);
 	}
